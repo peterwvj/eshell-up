d758a8271d4a8666a3d822dfa7bba8e3ae7a4c50
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-10-18 23:29:36 +0200

    Update version number to 0.0.2

--- eshell-up.el
+++ eshell-up.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Peter W. V. Tran-Jørgensen <peter.w.v.jorgensen@gmail.com>
 ;; URL: https://github.com/peterwvj/eshell-up
 ;; Created: 14th October 2016
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: eshell
 
